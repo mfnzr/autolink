eb7b77173de2efdf704d0721211eb3314922daef
Maria Fernanda Ribeiro <mfnanda.ribeiro@gmail.com>
2025-06-09 22:48:31 -0300

    viteconfig

--- autolink/vite.config.ts
+++ autolink/vite.config.ts
@@ -1,5 +1,7 @@
-// @ts-nocheck
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
-module.exports = {
-  publicPath: '/autolink/',
-}
+export default defineConfig({
+  base: '/autolink/',
+  plugins: [vue()],
+})
