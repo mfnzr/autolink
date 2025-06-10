d0f2b6f20e3063b219fbc30c6a25794c0ef2b8c7
Maria Fernanda Ribeiro <mfnanda.ribeiro@gmail.com>
2025-06-09 23:02:30 -0300

    tentando fazer deploy

--- autolink/vite.config.ts
+++ autolink/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
 
 export default defineConfig({
   base: '/autolink/',
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')  // Isso faz o '@' apontar para a pasta src
+    }
+  }
 })
